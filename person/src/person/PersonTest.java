c68b9912efe3e9b7987f96e57a76df18da3afca3
alfonso domenici <alfonso.domenici@gmail.com>
2024-03-15 17:24:59 +0100

    test

---
+++ person/src/person/PersonTest.java
@@ -0,0 +1,37 @@
+package person;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class PersonTest {
+    @Test
+    void testEquals() {
+        assertEquals(new Person("z", 20), new Person("z", 20));
+    }
+
+    @Test
+    void testGetAge() {
+
+    }
+
+    @Test
+    void testGetName() {
+
+    }
+
+    @Test
+    void testSetAge() {
+
+    }
+
+    @Test
+    void testSetName() {
+
+    }
+
+    @Test
+    void testToString() {
+
+    }
+}
